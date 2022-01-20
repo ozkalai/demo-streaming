dfb0e9f671cff51e36dd0c215db517288e612fca
ozkalai <melihozkalayy@gmail.com>
2022-01-20 13:14:22 -0800

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'blue': '#017EFE',
-      'gray': '#414141',
-      'light-gray': '#D8D8D8',
-      'black': '#1E1E1E',
-      'white': "#FFFFFF"
+    extend: {
+      colors: {
+        'blue': '#017EFE',
+        'gray': '#414141',
+        'lightGray': '#D8D8D8',
+        'black': '#1E1E1E',
+        'white': "#FFFFFF"
+      },
     },
+    
     fontFamily: {
       'raleway': ['raleway', 'sans-serif'],
     },
