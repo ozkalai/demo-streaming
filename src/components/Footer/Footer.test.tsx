aa64a7144f4ec78c0de7ea9704d577c4bd4fa103
ozkalai <melihozkalayy@gmail.com>
2022-01-20 07:23:21 -0800

    Footer Component Initial

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from ".";
+
+describe("test demo",() => {
+    test("test", () => {
+        render(<Footer />)
+
+        const footerText = screen.getByText("Footer")
+
+        expect(footerText).toBeTruthy()
+    })
+})
