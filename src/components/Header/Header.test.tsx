bad6dad9a10213864f7723dc88bcc6d89d69d3ea
ozkalai <melihozkalayy@gmail.com>
2022-01-21 03:58:44 -0800

    Header component with style and Unit test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,12 +1,24 @@
 import { render, screen } from "@testing-library/react";
-import { Header } from ".";
+import { createMockRouter } from "../../../__mocks__/createMockRouter";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 
-describe("test demo",() => {
-    test("test", () => {
-        render(<Header />)
+import { Header } from "@/components/Header";
 
-        const footerText = screen.getByText("Header")
+describe("Header", () => {
+  test("it render header title", () => {
+    render(
+      <RouterContext.Provider
+        value={createMockRouter({
+          query: {
+            id: "1",
+          },
+        })}
+      >
+        <Header />
+      </RouterContext.Provider>
+    );
+    const headerMainTitle = screen.getByText("DEMO Streaming");
 
-        expect(footerText).toBeTruthy()
-    })
-})
+    expect(headerMainTitle).toBeTruthy();
+  });
+});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,51 @@
-import { TextButton } from "../TextButton";
-import styles from "./Header.module.scss";
+import { useRouter } from "next/router";
 
-export const Header = () => {
+import { Button } from "@/components/Button";
+import { TextButton } from "@/components/TextButton";
+
+import styles from "./Header.module.scss";
+import { useEffect, useState } from "react";
+
+export const Header: React.FC = (): JSX.Element => {
+  const { pathname } = useRouter();
+
+  const [headerTitle, setHeaderTitle] = useState("Popular Titles");
+
+  const getHeaderTitle = (pathname: string) => {
+    switch (pathname) {
+      case "/movies":
+        return "Popular Movies";
+        break;
+      case "/series":
+        return "Popular Series";
+        break;
+      default:
+        return "Popular Title";
+        break;
+    }
+  };
+
+  useEffect(() => {
+    setHeaderTitle(getHeaderTitle(pathname));
+  }, []);
+
   return (
-    <div className={styles.header}>
-      <TextButton text="Log In" className="text-white" />
+    <div data-test-id="header-test" className={styles.header}>
+      <div className={styles.headerMain}>
+        <div className={styles.headerMainContent}>
+          <h1 className={styles.headerMainContentName}>DEMO Streaming</h1>
+          <div className={styles.headerMainContentButtons}>
+            <TextButton className="text-white" text="Log In" />
+            <Button
+              className="text-white p-2 bg-gray"
+              text="Start your free trial"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.headerTitle}>
+        <div className={styles.headerTitleContent}>{headerTitle}</div>
+      </div>
     </div>
   );
 };
