5129f72d7e64d10146c5b89598a1b30cdad1ab65
ozkalai <melihozkalayy@gmail.com>
2022-01-20 23:52:29 -0800

    TestButton Unit test

---
+++ src/components/TextButton/TextButton.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { TextButton } from ".";
+
+describe("TextButton", () => {
+  test("it render text ", () => {
+    render(<TextButton text="test" />);
+
+    const buttonText = screen.getByText("test");
+
+    expect(buttonText).toHaveTextContent("test");
+  });
+
+  test("it render text color ", () => {
+    render(<TextButton className="text-black" text="styled text button" />);
+
+    const styledTextButton = screen.getByText("styled text button");
+
+    expect(styledTextButton).toHaveClass("text-black");
+  });
+});
