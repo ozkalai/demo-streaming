1ba94d6333d356f48774a5870296d4b7d5ef4be4
ozkalai <melihozkalayy@gmail.com>
2022-01-22 08:31:44 -0800

    Custom 404 page instead of error page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import { Content } from "@/components/Content";
+
+export default function Custom404() {
+  return (
+    <Content>
+      <h1>Oops, something went wrong...</h1>
+    </Content>
+  );
+}
