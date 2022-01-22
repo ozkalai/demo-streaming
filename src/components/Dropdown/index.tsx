cd51d1105c9a8718938feb6f8ebc76b9993b8a0b
ozkalai <melihozkalayy@gmail.com>
2022-01-22 14:07:22 -0800

    Dropdown component with style

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { FaSortDown } from "react-icons/fa";
+
+import { IDropdownProps } from "@/interfaces/Dropdown";
+import styles from "./Dropdown.module.scss";
+
+export const Dropdown: React.FC<IDropdownProps> = ({
+  optionsList,
+}): JSX.Element => {
+  const [headerTitle, setHeaderTitle] = useState("Sort By");
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log("optionList", optionsList);
+
+  return (
+    <div className={styles.dropdown}>
+      <div
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        className={styles.dropdownHeader}
+      >
+        <div className={styles.dropdownHeaderTitle}>{headerTitle}</div>
+        <FaSortDown className={styles.dropdownHeaderIcon} />
+      </div>
+      {isOpen && (
+        <div className={styles.dropdownList}>
+          {optionsList.map((o, i) => (
+            <div className={styles.dropdownListItem} key={i + 1}>
+              <button
+                onClick={() => {
+                  setHeaderTitle(o.title);
+                  setIsOpen(false);
+                  o.cb();
+                }}
+                className={styles.dropdownListItemButton}
+              >
+                {o.title}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
