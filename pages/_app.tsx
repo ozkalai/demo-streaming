779af56255dfb9db6b63b4d6fe9b64cb956ea43d
ozkalai <melihozkalayy@gmail.com>
2022-01-22 04:03:47 -0800

    push error page when pathname equal error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,21 @@ import { Header } from "@/components/Header";
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { pathname, push } = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/_error") {
+      push("/error");
+    }
+  }, [pathname]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
