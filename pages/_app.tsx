1262ffae21d7524fc73eb07ee5428c29540b5282
ozkalai <melihozkalayy@gmail.com>
2022-01-23 02:48:22 -0800

    fixed lack of dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     if (pathname === "/_error") {
       push("/error");
     }
-  }, [pathname]);
+  }, [pathname, push]);
 
   return (
     <QueryClientProvider client={queryClient}>
