da05c00e7cf2662c55c8ea8f33b514b1134fb3b0
ozkalai <melihozkalayy@gmail.com>
2022-01-23 02:57:44 -0800

    pages [WIP]

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 import { useQuery } from "react-query";
 
 import { Content } from "@/components/Content";
-import { ProgramCard } from "@/components/ProgramCard";
-import { IProgramData } from "@/interfaces/ProgramData";
 import styles from "@/styles/pages/Movies.module.scss";
 import { Search } from "@/components/Search";
+import { Dropdown } from "@/components/Dropdown";
+import { ProgramList } from "@/components/ProgramList";
 
 const Movies: NextPage = () => {
   const { isLoading, error, data } = useQuery("repoData", () =>
@@ -14,21 +14,47 @@ const Movies: NextPage = () => {
       .then((data) => data.entries)
   );
 
+  const optionList = [
+    {
+      title: "Sort by year in descending order.",
+      cb: () => {
+        console.log("clicked first item");
+      },
+    },
+    {
+      title: "Sort by year in ascending order.",
+      cb: () => {
+        alert("clicked second item");
+      },
+    },
+    {
+      title: "Sort by title in descending order.",
+      cb: () => {
+        console.log("clicked third item");
+      },
+    },
+    {
+      title: "Sort by title in ascending order.",
+      cb: () => {
+        console.log("clicked forth item");
+      },
+    },
+  ];
+
   return (
     <Content>
       <main className={styles.movies}>
-        <Search />
-        <br></br>
+        <div className="grid grid-cols-7 gap-4">
+          <div className="col-start-1 col-span-3 ...">
+            <Search onClick={() => {}} />
+          </div>
+          <div className="col-start-4 col-span-2"></div>
+          <div className="col-start-6 col-span-2">
+            <Dropdown optionsList={optionList} />
+          </div>
+        </div>
         {error ? "Oops, something went wrong" : ""}
-        {isLoading ? (
-          "Loading"
-        ) : (
-          <ul className={styles.moviesList}>
-            {data?.map((p: IProgramData) => (
-              <ProgramCard programData={p} key={p.title} />
-            ))}
-          </ul>
-        )}
+        {isLoading ? "Loading" : <ProgramList data={data} />}
       </main>
     </Content>
   );
