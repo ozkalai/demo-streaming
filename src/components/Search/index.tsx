4aada4aa0e3fef554b3aafc233de566312259b5b
ozkalai <melihozkalayy@gmail.com>
2022-01-23 02:43:16 -0800

    Search prop updated

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,19 +1,15 @@
 import { FaSearch } from "react-icons/fa";
 
+import { ISearchProps } from "@/interfaces/Search";
 import styles from "./Search.module.scss";
 
-export const Search: React.FC = (): JSX.Element => {
+export const Search: React.FC<ISearchProps> = ({ onClick }): JSX.Element => {
   return (
     <div className={styles.search}>
       <div className={styles.searchInput}>
         <input placeholder="Search" />
       </div>
-      <button
-        onClick={() => {
-          console.log("clicked");
-        }}
-        className={styles.searchButton}
-      >
+      <button onClick={onClick} className={styles.searchButton}>
         <FaSearch data-testid="icon" />
       </button>
     </div>
