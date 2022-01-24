da4b2a03d6726eb24f0a07cea0798ac3a77b88b0
ozkalai <melihozkalayy@gmail.com>
2022-01-23 22:34:43 -0800

    configs

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ const customJestConfig = {
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
-
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
