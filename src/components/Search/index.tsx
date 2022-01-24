3861f6c34ca18a06a75d32cc5f60b2e0e0c7e703
ozkalai <melihozkalayy@gmail.com>
2022-01-23 22:40:34 -0800

    Search updated

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 import { ISearchProps } from "@/interfaces/Search";
 import styles from "./Search.module.scss";
 
 export const Search: React.FC<ISearchProps> = ({ onClick }): JSX.Element => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className={styles.search}>
       <div className={styles.searchInput}>
-        <input placeholder="Search" />
+        <input onChange={(e) => setQuery(e.target.value)} value={query} placeholder="Search" />
       </div>
-      <button onClick={onClick} className={styles.searchButton}>
+      <button
+        onClick={() => {
+          onClick(query);
+        }}
+        className={styles.searchButton}
+      >
         <FaSearch data-testid="icon" />
       </button>
     </div>
