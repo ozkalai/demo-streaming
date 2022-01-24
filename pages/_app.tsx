6cf0702f3aff3ead6d533c0f7f648ec9b0892e62
ozkalai <melihozkalayy@gmail.com>
2022-01-23 22:34:29 -0800

    removed unused

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.scss";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { pathname, push } = useRouter();
-
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
