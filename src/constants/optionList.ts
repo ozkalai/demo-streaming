66fa9dc30b051ad67ab9f5391d7bcccb49e1a8fd
ozkalai <melihozkalayy@gmail.com>
2022-01-23 22:40:09 -0800

    optionList

---
+++ src/constants/optionList.ts
@@ -0,0 +1,18 @@
+export const optionList = [
+  {
+    label: "Sort by year in descending order.",
+    value: "year-desc",
+  },
+  {
+    label: "Sort by year in ascending order.",
+    value: "year-asc",
+  },
+  {
+    label: "Sort by title in descending order.",
+    value: "title-desc",
+  },
+  {
+    label: "Sort by title in ascending order.",
+    value: "title-asc",
+  },
+];
