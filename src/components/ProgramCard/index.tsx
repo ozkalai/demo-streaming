bd7a0f712e4574a14b99fce4dea50726720d5ff4
ozkalai <melihozkalayy@gmail.com>
2022-01-25 22:39:56 -0800

    ProgramCard linked to detail path

--- src/components/ProgramCard/index.tsx
+++ src/components/ProgramCard/index.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { IProgramCard } from "@/interfaces/ProgramCard";
 import styles from "./ProgramCard.module.scss";
 
 export const ProgramCard: React.FC<IProgramCard> = ({ programData }): JSX.Element => {
   return (
-    <div className={styles.programCard}>
-      <div className={styles.programCardImage}>
-        <Image
-          width={130}
-          height={201}
-          layout="responsive"
-          src={programData.images["Poster Art"].url}
-          alt="Program Image"
-        />
+    <Link passHref href={`/program/${programData.id}`}>
+      <div className={styles.programCard}>
+        <div className={styles.programCardImage}>
+          <Image
+            width={130}
+            height={201}
+            layout="responsive"
+            src={programData.images["Poster Art"].url}
+            alt="Program Image"
+          />
+        </div>
+        <div className={styles.programCardTitle}>{programData.title}</div>
       </div>
-      <div className={styles.programCardTitle}>{programData.title}</div>
-    </div>
+    </Link>
   );
 };
